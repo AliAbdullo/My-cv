d7259c334d1d5a53f82c808235bb9f7f802bfda9
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-11-20 20:25:35 +0500

    Update home.py

--- home.py
+++ home.py
@@ -1,12 +1,3 @@
-#from django_filters.rest_framework import DjangoFilterBackend
-#from rest_framework.generics import get_object_or_404
-#from rest_framework.permissions import IsAdminUser
-#from rest_framework.filters import SearchFilter
-#from rest_framework.response import Response
-#from rest_framework.views import APIView
-#from rest_framework import generics
-#from .permissions import IsAdminOrCreateOnly
-#from .filters import DateRangeFilterBackend
 
 # Print funksiyasi
 # Mualif Men
